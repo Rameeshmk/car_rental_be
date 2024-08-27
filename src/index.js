4e19404ac5c27701e87ce4c39ca09603ac61fc77
Rameeshmk <rameeshmk@gmail.com>
2024-08-27 17:22:09 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -10,7 +10,7 @@ const app = express()
 app.use(
     cors(
         {
-    origin:["https://car-rental-web-fe-new1-fnl.vercel.app/","http://localhost:5173"],
+    origin:"*",//["https://car-rental-web-fe-new1-fnl.vercel.app/","http://localhost:5173"],//
 
 
     credentials : true,
