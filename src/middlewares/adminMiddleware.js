ae8b3e1c4acfd7b347d06bf53512d7cd1de5c4da
Rameeshmk <rameeshmk@gmail.com>
2024-08-28 05:42:47 +0300

    Update adminMiddleware.js

--- src/middlewares/adminMiddleware.js
+++ src/middlewares/adminMiddleware.js
@@ -3,8 +3,10 @@ import serverConfig from "../config/serverConfig.js"
 
 const authenticateAdmin = (req,res,next)=>{
 
-    const token = req.cookies.token
-
+    const token = req.cookies.token;
+    if (!token) {
+        return res.status(401).send("No token provided");
+    }
     jwt.verify(token,serverConfig.token, (err, result)=>{
         if (err){
             console.log(err);
