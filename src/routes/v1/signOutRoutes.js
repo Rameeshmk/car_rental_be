dd26c1bc3df011d276b119e1360cc181a3cd621c
Rameeshmk <rameeshmk@gmail.com>
2024-08-30 06:43:16 +0300

    change sign out backend

--- src/routes/v1/signOutRoutes.js
+++ src/routes/v1/signOutRoutes.js
@@ -3,12 +3,13 @@ import express from 'express';
 const signOutRouter = express.Router();
 
 signOutRouter.post('/signout', (req, res) => {
-  
-  res.clearCookie('token');
-  res.clearCookie('refreshToken');
+  // Clear cookies
+  res.clearCookie('token', { path: '/' });
+  res.clearCookie('refreshToken', { path: '/' });
 
-  
-  res.status(200).json({ message: 'Sign out successful' });
+  // Optionally, handle any server-side session or token invalidation here
+
+  res.status(200).json({ message: 'Signed out successfully' });
 });
 
 export default signOutRouter;
