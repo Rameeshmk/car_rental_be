9a3f757cf2d1c577c21eeedd0cd74f9fc7c0689e
Rameeshmk <rameeshmk@gmail.com>
2024-09-05 17:05:10 +0300

    Update orderController.js

--- src/controllers/orderController.js
+++ src/controllers/orderController.js
@@ -110,7 +110,7 @@ const checkAvailability = async (req, res) => {
 
 const getDealersOrders = async (req, res) => {
   try {
-    const { carId } = req.params;
+    const { carId } = req.user;
 
     if (!carId) {
       return res.status(400).json({ error: 'Car ID is required' });
