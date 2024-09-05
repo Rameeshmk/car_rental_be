b8bd1f3ab337d275d9729a63dbb725d37762f1d2
Rameeshmk <rameeshmk@gmail.com>
2024-09-05 05:16:13 +0300

    Update carModels.js

--- src/models/carModels.js
+++ src/models/carModels.js
@@ -39,7 +39,7 @@ const carSchema = new mongoose.Schema(
     image: {
       type: String,
     },
-    dealer: [{dealerId:{ type: mongoose.Types.ObjectId, ref: "dealer" }}],
+    dealer: [{ type: mongoose.Types.ObjectId, ref: "dealer" }],
   },
   { timestamps: true }
 );
