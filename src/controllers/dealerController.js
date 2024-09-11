560b75d0da5a485fbf03e8be90f20b841f0a75d6
Rameeshmk <rameeshmk@gmail.com>
2024-09-11 15:19:37 +0300

    Update dealerController.js

--- src/controllers/dealerController.js
+++ src/controllers/dealerController.js
@@ -75,7 +75,7 @@ const singin = async (req, res) => {
       sameSite: isProduction ? "None": "Lax",
     
     });
-    return res.json({ message: "Logged in!", token, userRole, dealerId: dealer._id });
+    return res.json({ message: "Logged in!", token, userRole, dealerId: dealer._id,isApproved:dealer.isApproved });
 
   } catch (error) {
     console.error("Error", error);
